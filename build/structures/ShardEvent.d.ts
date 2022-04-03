f0914e99ac962b1f82860102817d7b81cb4b0aa2
Qreepex <65496825+Qreepex@users.noreply.github.com>
2022-04-03 21:30:14 +0200

    Removed build

--- build/structures/ShardEvent.d.ts
+++
@@ -1,6 +0,0 @@
-import Event from "./Event";
-import Bot from "./Bot";
-export default class ShardEvent extends Event {
-  constructor(name: string, client: Bot);
-  get isShardEvent(): boolean;
-}
--- build/utils/Logger.d.ts
+++
@@ -1,8 +0,0 @@
-import { Console } from "console";
-export default class Logger extends Console {
-  constructor();
-  info(input: string, type?: string): void;
-  error(input: string): void;
-  warn(input: string): void;
-  date(msTimeStamp?: number): string;
-}
