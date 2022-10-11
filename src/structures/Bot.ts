8fceaab11db8fa62c68b6590d0657f87f7ca6b38
qreepex <dev@qreepex.xyz>
2022-10-11 16:50:53 +0200

    build(npm): add new translator

--- src/structures/Bot.ts
+++ src/structures/Bot.ts
@@ -97,7 +97,7 @@ class Bot {
     this.commands = new CommandCollection(this);
     await this.commands.loadCommands();
 
-    this._i18n = new Translator(this._config.weblate_token);
+    this._i18n = new Translator(this._config.weblate_token, this._logger);
     await this._i18n.loadMessages();
 
     this.cooldowns = new CooldownsManager(this);
